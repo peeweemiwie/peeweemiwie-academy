ce8df86ec2251c1a8c1505b14cea7517623fe086
peeweemiwie <miwayagi@gmail.com>
2016-06-05 18:25:32 -0400

    Create a new gulpfile.js to test for portfolio redesign.

--- _portfolio-redesign/gulpfile.js
+++ _portfolio-redesign/gulpfile.js
@@ -1,14 +1,17 @@
-var gulp        = require('gulp'),
-    browserSync = require('browser-sync').create(),
-    sass        = require('gulp-sass'),
-    useref      = require('gulp-useref'),
-    uglify      = require('gulp-uglify'),
-    gulpIf      = require('gulp-if'),
-    cssnano     = require('gulp-cssnano'),
-    imagemin    = require('gulp-imagemin'),
-    cache       = require('gulp-cache'),
-    del         = require('del'),
-    runSequence = require('run-sequence');
+var gulp         = require('gulp'),
+    browserSync  = require('browser-sync').create(),
+    sass         = require('gulp-sass'),
+    useref       = require('gulp-useref'),
+    uglify       = require('gulp-uglify'),
+    gulpIf       = require('gulp-if'),
+    cssnano      = require('gulp-cssnano'),
+    imagemin     = require('gulp-imagemin'),
+    cache        = require('gulp-cache'),
+    del          = require('del'),
+    runSequence  = require('run-sequence'),
+    postcss      = require('gulp-postcss'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer');
     
 
 gulp.task('browserSync', function(){
@@ -19,7 +22,17 @@ gulp.task('browserSync', function(){
   });
 });
 
-gulp.task('watch', ['browserSync'], function(){
+gulp.task('sass', function(){
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function(){
+  gulp.watch('app/scss/**/*.scss', ['sass']);
   gulp.watch('app/*.html', browserSync.reload);
   gulp.watch('app/js/**/*.html', browserSync.reload);
   gulp.watch('app/js/**/*.js', browserSync.reload)
@@ -32,15 +45,20 @@ gulp.task('useref', function(){
     .pipe(gulp.dest('dist'))
 });
 
-
-
 gulp.task('clean:dist', function(){
   return del.sync('dist');
 });
 
+
+
+// gulp.task('autoprefixer', function () {
+//     return gulp.src('app/scss/**/*.scss')
+//       .pipe(sourcemaps.init())
+//       .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+//       .pipe(sourcemaps.write('.'))
+//       .pipe(gulp.dest('app/css'))
+// });
+
 gulp.task('default', function(){
-  runSequence(['browserSync', 'watch']);
+  runSequence(['sass', 'browserSync', 'watch']);
 });
-
-
-
