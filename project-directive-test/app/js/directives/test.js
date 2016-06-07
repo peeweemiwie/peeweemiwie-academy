2bf4cb03d8485bf17cc22c532de9620ed7036d57
peeweemiwie <miwayagi@gmail.com>
2016-06-07 12:15:13 -0400

    Clone angular-phonecat. create directive test links

--- project-directive-test/app/js/directives/test.js
+++ project-directive-test/app/js/directives/test.js
@@ -1,11 +1,17 @@
 app.directive('directiveTest', function(){
   return {
     restrict: 'E',
-    templateUrl: "js/directives/test.html",
+    templateUrl: "js/directives/test.html"
+  };
+});
+
+app.directive('testLink', function(){
+  return {
+    templateUrl: 'js/directives/link.html',
     link: function(scope, element, attr){
       scope.like = function(){
         element.toggleClass('btn-like');
       };
     }
-  };
+  }
 });
